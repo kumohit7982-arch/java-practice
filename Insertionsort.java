54a4cf1be2e6ca41b8c074455925cf3bf6dabf0b
Mohit kumar <kumohit7982@gmail.com>
2026-04-14 11:34:17 +0530

    Create Insertionsort.java

---
+++ Insertionsort.java
@@ -0,0 +1,23 @@
+public class Insertionsorrt {
+    public static void printArray(int arr[]){
+        for (int i=0; i< arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]= {5,3,6,3,6,7 };
+
+        for (int i=0; i< arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j]>current){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+        printArray(arr);
+
+    }
+}
